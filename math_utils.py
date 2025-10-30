585816ef6608c39fef3ae9410a63f95a82433348
Tzvi Sender <gr58cw5c59@privaterelay.appleid.com>
2025-10-30 13:36:33 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -7,8 +7,8 @@ def find_max_number(num1, num2, num3):
     else
      if(num2>num3):
         return num2
-num1 = 1
-num2 = 3
-num3 = -1
+num1 =
+num2 = 
+num3 = 
 max_num = find_max_number(num1, num2, num3)
 print("The maximum number is:", max_num)
