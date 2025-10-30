9828bcb2070b4439bd46b4e597265de877c8d5e2
Tzvi Sender <gr58cw5c59@privaterelay.appleid.com>
2025-10-30 13:31:56 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,10 +1,14 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
-
-def find_mean(num1, num2, num3):
-    pass  # Replace 'pass' with code
-
-def find_mean_std(num1, num2, num3):
-    mean = find_mean(num1, num2, num3)
-    pass  # Replace 'pass' with code
-
+    if (num1>num2) :
+      if (num1>num3):
+        return num1
+      else
+        return num3
+    else
+     if(num2>num3):
+        return num2
+num1 = 1
+num2 = 3
+num3 = -1
+max_num = find_max_number(num1, num2, num3)
+print("The maximum number is:", max_num)
